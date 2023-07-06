116188e7366007be3d11382a0feba2ae669ff57f
mudassirarif17 <mudassirinoxent@gmail.com>
2023-07-06 16:48:54 +0500

    Bottom tab navigation is configure

--- App.js
+++ App.js
@@ -1,12 +1,40 @@
 import React from 'react'
 import { View , Text } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+
+const Tab = createBottomTabNavigator();
 
 const App = () => {
   return (
-    <View style={{flex: 1 , justifyContent: "center" , alignItems: "center"}}>
-      <Text style={{fontSize : 30 }}>Tab Navigation</Text>
+    <NavigationContainer>
+    <Tab.Navigator>
+      <Tab.Screen name="Chat" component={Chat} />
+      <Tab.Screen name="Status" component={Status} />
+      <Tab.Screen name="Calls" component={Calls} />
+    </Tab.Navigator>
+    </NavigationContainer>
+  )
+}
+const Chat = () =>{
+  return(
+    <View style={{flex : 1, justifyContent : "center" , alignItems : "center"}}>
+      <Text style={{fontSize : 30}}>Chat</Text>
     </View>
   )
 }
-
+const Calls = () =>{
+  return(
+    <View style={{flex : 1, justifyContent : "center" , alignItems : "center"}}>
+      <Text style={{fontSize : 30}}>Calls</Text>
+    </View>
+  )
+}
+const Status = () =>{
+  return(
+    <View style={{flex : 1, justifyContent : "center" , alignItems : "center"}}>
+      <Text style={{fontSize : 30}}>Status</Text>
+    </View>
+  )
+}
 export default App;
