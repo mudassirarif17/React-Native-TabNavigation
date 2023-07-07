e7ea0d5fda993058f2a59c25b5690877f29d4f85
mudassirarif17 <mudassirinoxent@gmail.com>
2023-07-07 09:31:43 +0500

    tab-top navigation is ready

--- App.js
+++ App.js
@@ -2,8 +2,11 @@ import React from 'react'
 import { View , Text } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { createMaterialTopTabNavigator } from '@react-navigation/material-top-tabs';
 
-const Tab = createBottomTabNavigator();
+
+// const Tab = createBottomTabNavigator();
+const Tab = createMaterialTopTabNavigator();
 
 const App = () => {
   return (
